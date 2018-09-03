0a3b4a5b5d146e5c10b388a08c3b1e7c4c616877
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-09-02 22:32:48 -0300

    feat: add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from "./app.routes";
+import { HeaderComponent } from './shared/components/header/header.component';
 import { PanelComponent } from './pages/panel/panel.component';
 import { HomeComponent } from './pages/panel/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PanelComponent,
     HomeComponent
   ],
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    // Auto Init all Materialize Components
+    M.AutoInit();
+  }
+
+}
