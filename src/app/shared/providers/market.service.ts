d5221ee6f2d37dc95e5f8a6bad7d984113fc018e
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-09-03 10:13:27 -0300

    refactor: Update API url as a variable

--- src/app/shared/providers/market.service.ts
+++ src/app/shared/providers/market.service.ts
@@ -6,18 +6,23 @@ import { map, switchMap } from "rxjs/operators";
 @Injectable()
 export class MarketService {
 
+	API_BITCOIN = "http://www.mercadobitcoin.net/api/BTC/ticker/"
+	API_BRITA = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='02-09-2018'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda"
+
 	constructor(private http: Http) { }
 
+	//Get object from API_BITCOIN, every 3 seconds
 	getBitcoin(): Observable<any> {
 		return interval(3000).pipe(
-			switchMap(() => this.http.get("http://www.mercadobitcoin.net/api/BTC/ticker/")
+			switchMap(() => this.http.get(this.API_BITCOIN)
 				.pipe(map(response => response.json().ticker)))
 		)
 	}
 
+	//Get object from API_BRITA, every 3 seconds
 	getBrita(): Observable<any> {
 		return interval(3000).pipe(
-			switchMap(() => this.http.get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='02-09-2018'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda")
+			switchMap(() => this.http.get(this.API_BRITA)
 				.pipe(map(response => response.json().value)))
 		)
 	}
