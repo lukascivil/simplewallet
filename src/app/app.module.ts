a34727f69112e86b26fa4421516e69e8606dbb53
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-09-02 22:30:29 -0300

    feat: add user homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from "./app.routes";
+import { PanelComponent } from './pages/panel/panel.component';
+import { HomeComponent } from './pages/panel/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
+import { PanelComponent } from "./pages/panel/panel.component";
+import { HomeComponent } from "./pages/panel/home/home.component";
 
-export const ROUTES: Routes = []
+export const ROUTES: Routes = [
+  {
+    path: "", component: PanelComponent,
+    children: [
+      { path: "", component: HomeComponent }
+    ]
+  }
+]
