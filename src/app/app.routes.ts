fd88a15848443227bcbed9b8ecbf36f87e247d61
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2018-09-04 15:53:53 -0300

    feat: add error404 path route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PanelComponent } from "./pages/panel/panel.component";
 import { HomeComponent } from "./pages/panel/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { registerComponent } from "./pages/register/register.component";
+import { Error404Component } from "./pages/error404/error404.component";
 
 export const ROUTES: Routes = [
   {
@@ -12,5 +13,6 @@ export const ROUTES: Routes = [
     ]
   },
   { path: "login", component: LoginComponent },
-  { path: "register", component: registerComponent }
+  { path: "register", component: registerComponent },
+  { path: "**", component: Error404Component }
 ]
